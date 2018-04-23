5939c1824e5f35a288a17a8002169e341b2308c1
zenyhoj <joe.balingit@gmail.com>
2018-04-23 16:29:05 +0800

    webpack initial commit: working

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    //mode: 'development',
+    entry: "./src/app.js", //absolute path
+    output: {
+        path: __dirname + "/public", //relative path; where the bundled file be located
+        filename: "bundle.js"
+    },
+    devServer: {
+        "contentBase": __dirname + "/public", //looks for index.html inside public dir
+        port: 3000,
+        hot: true
+    }
+   
+
+};
